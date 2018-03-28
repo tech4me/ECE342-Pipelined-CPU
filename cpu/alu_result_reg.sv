3e1b1ada9f99dc08321510f4a7c2643ab023b3a1
tech4me <guiwanglong@gmail.com>
2018-03-28 18:52:22 -0400

    Added alu_result_reg

---
+++ cpu/alu_result_reg.sv
@@ -0,0 +1,17 @@
+module alu_result_reg (
+    input clk,
+    input rst,
+    input enable,
+    input [15:0] reg_in,
+    output logic [15:0] reg_out
+);
+
+always_ff @(posedge clk) begin
+    if (rst) begin
+        reg_out <= 0; 
+    end else if (enable) begin
+        reg_out <= reg_in;
+    end  
+end
+    
+endmodule
